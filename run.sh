2f1d89044bbe370c9f5639d0a0245de8820c88fd
Alex Klibisz <aklibisz@gmail.com>
2020-05-23 21:00:30 -0400

    Setup to use joblog and got rid of --bar for eager evaluation

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -e
 
 bucket=${bucket:-elastiknn-benchmarks}
-prefix=${prefix:-data/amazon-reviews/images}
+prefix=${prefix:-data/raw/amazon-reviews/images}
 n=${n:-1000}
 skip=${skip:-$(aws s3api list-objects --bucket $bucket --prefix $prefix --output json --query "length(Contents[])" || echo 0)}
 jobs=${jobs:-$(grep -c processor /proc/cpuinfo)}
@@ -10,5 +10,5 @@ jobs=${jobs:-$(grep -c processor /proc/cpuinfo)}
 echo "Skipping first $skip images. Reading next $n images with parallelism $jobs"
 
 python3 -u catamz.py $1 $skip $n \
-  | parallel --jobs $jobs --bar --retries 20 --halt now,fail=1000 --colsep ' ' \
-  "./single.sh {1} $bucket $prefix/{2}.jpg"
+  | parallel --jobs $jobs --joblog $1.log --resume --retries 5 --halt now,fail=1000 --colsep ' ' \
+  "./single.sh {1} $bucket $prefix/{2}.jpg && echo {#} {}"
